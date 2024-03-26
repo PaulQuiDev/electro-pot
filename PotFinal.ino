02bf706e785d1fe579445bb794c6cc2ae4a9d1aa
PaulQuiDev <80120197+PaulQuiDev@users.noreply.github.com>
2024-03-26 19:46:45 +0100

    Final Code
    The code made detects the rate of humidity of the sensor you indicate with the LED if humidity is good

--- PotFinal.ino
+++ PotFinal.ino
@@ -1,62 +1,59 @@
-const int signalPin = 5;    // Pin de sortie pour le signal carré
-const int measurePin = 4;   // Pin d'entrée pour mesurer l'amplitude
-const int LedR = 2; // pin led rouge
-const int LedV = 1; // pin led verte
+// Constants declaration
+const int signalPin = 5;    // Output pin for square wave signal
+const int measurePin = 4;   // Input pin to measure amplitude
+const int LedR = 2; // Red LED pin
+const int LedV = 1; // Green LED pin
 
 void setup() {
+  // Set pin modes
   pinMode(signalPin, OUTPUT);
   pinMode(measurePin, INPUT);
   pinMode(LedR, OUTPUT);
   pinMode(LedV, OUTPUT);
-  Serial.begin(115200);  // Initialisation de la communication série
+  // Initialize serial communication
+  Serial.begin(115200);
 }
 
-void loop() 
-{
-    digitalWrite(LedR,LOW);// signaler scan lancer
+void loop() {
+    digitalWrite(LedR,LOW); // Indicate scan initiation
     
-    digitalWrite(signalPin, HIGH);
-    int humiditer = analogRead(measurePin);
+    digitalWrite(signalPin, HIGH); // Generate square wave signal
+    int humidity = analogRead(measurePin); // Read humidity value
     
-    for (int i = 0 ; i < 10 ; i++)    
-    {
-      humiditer = int((humiditer + analogRead(measurePin))/2); // moyen empirique sur 10 valeur
+    // Compute average humidity over 10 readings
+    for (int i = 0; i < 10; i++) {
+      humidity = int((humidity + analogRead(measurePin))/2); // Empirical averaging over 10 readings
       delayMicroseconds(50);
     }
-    digitalWrite(signalPin, LOW);
+    digitalWrite(signalPin, LOW); // End square wave signal
     
-    Serial.print("humiditer = ");
-    Serial.println(humiditer);
+    // Print humidity value to serial monitor
+    Serial.print("Humidity = ");
+    Serial.println(humidity);
 
-    // comunication vers homme
-    if (humiditer > 1100)
-    {
-      digitalWrite(LedR,HIGH);
+    // Alert user based on humidity levels
+    if (humidity > 1100) {
+      digitalWrite(LedR, HIGH); // Turn on red LED for 1 second
       delay(1000);
-      digitalWrite(LedR,LOW);
-    }
-    else 
-    {
-      if (humiditer < 400)
-      {
-        for (int i = 0 ; i < 15 ; i++)
-        {
-          digitalWrite(LedR,HIGH);
+      digitalWrite(LedR, LOW); // Turn off red LED
+    } else {
+      if (humidity < 400) {
+        // Blink red LED rapidly if humidity is low
+        for (int i = 0; i < 15; i++) {
+          digitalWrite(LedR, HIGH);
           delay(50);
-          digitalWrite(LedR,LOW);
+          digitalWrite(LedR, LOW);
           delay(50);
         }
-          
-      }
-      else
-      { 
-        digitalWrite(LedV,LOW);
-        digitalWrite(LedV,HIGH);
+      } else {
+        // If humidity is within normal range, blink green LED once
+        digitalWrite(LedV, LOW);
+        digitalWrite(LedV, HIGH);
         delay(1000);
-        digitalWrite(LedV,LOW);
+        digitalWrite(LedV, LOW);
       }
     }
   
-  //Serial.println(amplitude);
-  delay(100);  // Pause d'une seconde entre chaque série de mesures
+  // Delay for a second before the next reading
+  delay(100);
 }
